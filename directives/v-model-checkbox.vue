d64df28737676ebd43008e8209cc2ee05f21400b
shunjid <shunjid.rahman@gmail.com>
2020-11-10 00:27:04 +0600

    added v-model examples

---
+++ directives/v-model-checkbox.vue
@@ -0,0 +1,51 @@
+<!-- Demo: https://cdpn.io/_shunjid/debug/OJXBvJV/LQAExgYZqJQr -->
+<template>
+  <div id="app">
+    <h3 class="default">Please check all that applies to your identity</h3>
+    <h4 class="default">{{ checkedNames }}</h4>
+    <span v-for="option in options" :key="option">
+      <input
+        v-model="checkedNames"
+        type="checkbox"
+        :id="option.value"
+        :value="option.value"
+      />
+      <label class="default" :for="option.value">{{
+        option.value.toUpperCase()
+      }}</label>
+      <br />
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      checkedNames: [],
+      options: [
+        {
+          value: "Asian",
+        },
+        {
+          value: "European",
+        },
+        {
+          value: "American",
+        },
+        {
+          value: "African",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap");
+
+.default {
+  font-family: "Roboto", sans-serif;
+}
+</style>
---
+++ directives/v-model.vue
@@ -0,0 +1,27 @@
+<!-- Demo: https://cdpn.io/_shunjid/debug/BazqYWe/mWAoNzvBEqvr -->
+<template>
+  <div id="app">
+    <h2 class="default">Type Here</h2>
+    <textarea v-model="message" rows="5" maxlength="72" />
+    <br />
+    <p class="default">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "This is a good place to type things !!!",
+    };
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2family=Roboto:wght@300&display=swap");
+
+.default {
+  font-family: "Roboto", sans-serif;
+}
+</style>
