bf1585a407d1be8d43d43bcaaba1d95697399791
shunjid <shunjid.rahman@gmail.com>
2020-11-10 13:35:33 +0600

    added v-for example on table

---
+++ directives/v-for-simple.vue
@@ -0,0 +1,67 @@
+<!-- Demo: https://cdpn.io/_shunjid/debug/abZReVO/gaMeYdOWgRPM -->
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="num in range" :key="num">Scout Group-{{ num }}</li>
+    </ul>
+
+    <table>
+      <tr>
+        <th>Teams</th>
+        <th>Country</th>
+        <th>Captain</th>
+      </tr>
+      <tr v-for="(country, index) in countries" :key="country.name">
+        <td>Team-{{ index + 1 }}</td>
+        <td>{{ country.name }}</td>
+        <td>{{ country.captain }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      countries: [
+        {
+          name: "🇧🇩",
+          captain: "Mashrafe Mortaza",
+        },
+        {
+          name: "🇮🇳",
+          captain: "Virat Kohli",
+        },
+        {
+          name: "🇵🇰",
+          captain: "Azhar Ali",
+        },
+        {
+          name: "🇱🇰",
+          captain: "Dimuth Karunaratne",
+        },
+        {
+          name: "🇦🇫",
+          captain: "Rashid Khan",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+    table {
+      width: 100%
+    }
+    table,
+    th {
+        border: 1px solid black;
+    }
+    table,
+    th,
+    td {
+        text-align: center;
+    }
+</style>
