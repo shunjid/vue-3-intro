637b22fec9c3640b3f5c9f69b5662f103406dd4e
shunjid <shunjid.rahman@gmail.com>
2020-11-10 17:21:07 +0600

    added v-html example

---
+++ directives/v-html.vue
@@ -0,0 +1,18 @@
+<!-- Demo: https://cdpn.io/_shunjid/debug/mdEQJXQ/DqADdYJNBoWA -->
+<template>
+  <div id="app">
+    <p v-html="tacos"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        tacos: `
+            <p>Hacking <button onclick="alert('yo !!');">the hackers</button> hack</p>
+        `
+    };
+  }
+};
+</script>
