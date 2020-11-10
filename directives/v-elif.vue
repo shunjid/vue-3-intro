f304f2af0210f30b388086f1ee53363d360482d8
shunjid <shunjid.rahman@gmail.com>
2020-11-10 11:59:25 +0600

    added v-if v-elif v-else conditions

---
+++ directives/v-elif.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h3>Do you love animals?</h3>
+    <input type="radio" name="yes" id="yes" value="yes" v-model="lovesAnimal" />
+    <label for="yes">Yes</label>
+
+    <input type="radio" name="no" id="no" value="no" v-model="lovesAnimal" />
+    <label for="no">No</label>
+
+    <br />
+    <p v-if="lovesAnimal === 'yes'">You're So Cute 🥰</p>
+    <p v-else-if="lovesAnimal === 'no'">Why !!! 😢</p>
+    <p v-else>Click on a button above !!!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lovesAnimal: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
