8ff94d54711961db2d753248fa15e477fb0b68b1
shunjid <shunjid.rahman@gmail.com>
2020-11-10 16:05:09 +0600

    added v-pre v-once usage

---
+++ directives/v-pre-v-once.vue
@@ -0,0 +1,24 @@
+<!-- Demo: https://cdpn.io/_shunjid/debug/OJXaPWx/mVkbGwbWLKpM -->
+<template>
+  <div id="app">
+    <input type="text" name="food" id="food" v-model="favouriteFood">
+    <br>
+    <span v-once>
+        Initally loaded once: {{ favouriteFood }}
+    </span>
+    <br>
+
+    <h3 v-pre>Dynamically loaded {{ favouriteFood }}</h3>
+    <pre>{{ $data }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      favouriteFood: 'Pizza'
+    };
+  }
+};
+</script>
